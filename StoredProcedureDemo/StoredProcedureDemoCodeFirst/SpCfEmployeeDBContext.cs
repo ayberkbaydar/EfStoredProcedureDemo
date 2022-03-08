4a81b5cb036e8a87b78c79ccb0f6d2ac394aa38a
Ayberk <ybrk01@gmail.com>
2022-03-08 11:40:34 +0300

    08-03
    08-03

---
+++ StoredProcedureDemo/StoredProcedureDemoCodeFirst/SpCfEmployeeDBContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StoredProcedureDemoCodeFirst
+{
+    public class SpCfEmployeeDBContext : DbContext
+    {
+        public DbSet<SpCfEmployee> spCfEmployees { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SpCfEmployee>().MapToStoredProcedures
+                (p => p.Insert(i => i.HasName("InsertSpCfEmployee")
+                                    .Parameter(n => n.Name, "EmployeeName")
+                                    .Parameter(n => n.Gender, "EmployeeGender")
+                                    .Parameter(n => n.Salary, "EmployeeSalary"))
+                    .Update(u => u.HasName("UpdateSpCfEmployee")
+                                    .Parameter(n => n.ID, "EmployeeID")
+                                    .Parameter(n => n.Name, "EmployeeName")
+                                    .Parameter(n => n.Gender, "EmployeeGender")
+                                    .Parameter(n => n.Salary, "EmployeeSalary"))
+                    .Delete(d => d.HasName("DeleteSpCfEmployee")
+                                    .Parameter(n => n.ID, "EmployeeID"))
+                    );
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ StoredProcedureDemo/StoredProcedureDemoCodeFirst/SpCfEmployeeRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoredProcedureDemoCodeFirst
+{
+    public class SpCfEmployeeRepository
+    {
+        SpCfEmployeeDBContext spCfEmployeeDBContext = new SpCfEmployeeDBContext();
+        public List<SpCfEmployee> GetSpCfEmployees()
+        {
+            return spCfEmployeeDBContext.spCfEmployees.ToList();
+        }
+        public void InsertSpCfEmployee(SpCfEmployee spCfEmployee)
+        {
+            spCfEmployeeDBContext.spCfEmployees.Add(spCfEmployee);
+            spCfEmployeeDBContext.SaveChanges();
+        }
+        public void UpdateSpCfEmployee(SpCfEmployee spCfEmployee)
+        {
+            SpCfEmployee spCfEmployeeToUpdate = spCfEmployeeDBContext.spCfEmployees.FirstOrDefault(x => x.ID == spCfEmployee.ID);
+            spCfEmployeeToUpdate.Name = spCfEmployee.Name;
+            spCfEmployeeToUpdate.Gender = spCfEmployee.Gender;
+            spCfEmployeeToUpdate.Salary = spCfEmployee.Salary;
+            spCfEmployeeDBContext.SaveChanges();
+        }
+        public void DeleteSpCfEmployee(SpCfEmployee spCfEmployee)
+        {
+            SpCfEmployee spCfEmployeeToDelete = spCfEmployeeDBContext.spCfEmployees.FirstOrDefault(x => x.ID == spCfEmployee.ID);
+            spCfEmployeeDBContext.spCfEmployees.Remove(spCfEmployeeToDelete);
+            spCfEmployeeDBContext.SaveChanges();
+        }
+
+    }
+}
